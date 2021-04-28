c89320a86d13f8d485f0db49c85fe0b4bd041a95
mohamedel0 <elidrissimohamed058@gmail.com>
2021-04-28 13:10:10 +0200

    commit

--- angular/karate/src/app/app.module.ts
+++ angular/karate/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { InstructeurComponent } from './instructeur/instructeur.component';
 import { InstructeurModule } from './instructeur/instructeur.module';
+import { PresidentComponent } from './president/president.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
-    InstructeurComponent
+    InstructeurComponent,
+    PresidentComponent
   ],
   imports: [
     BrowserModule,
