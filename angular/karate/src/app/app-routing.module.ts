b1bee6698c437b9caffda4c14dbb5770f22a6884
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-04-28 14:06:57 +0200

    routes

--- angular/karate/src/app/app-routing.module.ts
+++ angular/karate/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
+import { InstructeurComponent } from './instructeur/instructeur.component';
+import { AddFormComponent as FormP } from './president/add-form/add-form.component';
+import { PresidentComponent } from './president/president.component';
+import { AddFormComponent as FormS} from './secretaire/add-form/add-form.component';
+import { SecretaireComponent } from './secretaire/secretaire.component';
 
 const routes: Routes = [
 
-  {path:'', component:HomepageComponent}
+  {path:'', component:HomepageComponent},
+  {path:'p', component:PresidentComponent,
+    children:[
+      {path:'a',component:FormP},
+    ]
+  },
+  {path:'s', component:SecretaireComponent,
+  children:[
+    {path:'a',component:FormS},
+  ]
+  }
 
 
 ];
--- angular/karate/src/app/president/president.module.ts
+++ angular/karate/src/app/president/president.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddFormComponent } from './add-form/add-form.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,6 +10,7 @@ import { AddFormComponent } from './add-form/add-form.component';
     AddFormComponent
   ],
   imports: [
+    RouterModule,
     CommonModule
   ]
 })
