6b719a4a309debccf83ed1f8175da720cde601cd
mohamedel0 <elidrissimohamed058@gmail.com>
2021-05-01 17:01:39 +0200

    commit

--- symfony/karate/src/DataFixtures/AppFixtures.php
+++ symfony/karate/src/DataFixtures/AppFixtures.php
@@ -2,10 +2,12 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Activite;
+use App\Entity\Categorie;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
 use App\Entity\Groupe;
+use App\Entity\InformationMedicale;
 use App\Entity\Membre;
 
 
@@ -15,14 +17,57 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
       $faker= \Faker\Factory::create('fr_FR');
-      for ($i=0;$i<4; $i++){
+      $activite= new Activite();
+      $categorie = new Categorie();
+      $infoMedicale = new InformationMedicale();
+
+      $categorie-> setNomCategorie('Mini poussins');
+      $categorie-> setNomCategorie('Poussins');
+      $categorie-> setNomCategorie('pupilles');
+      $categorie-> setNomCategorie('Benjamins');
+      $categorie-> setNomCategorie('Minimes');
+      $categorie-> setNomCategorie('Cadets');
+      $categorie-> setNomCategorie('Juniors');
+      $categorie-> setNomCategorie('Espoirs');
+      $categorie-> setNomCategorie('Séniors');
+      
+      
+        
+        
+        
+        
+        
+        
+        
+
+          
+
+      for ($i=0;$i<10; $i++){
           $membre=new Membre();
-          $membre->setNom($faker->word)
-                 ->setPrenom($faker->word);
-                 
+          $membre->setNumLicenceFFK($faker->word)
+                 ->setNom($faker->lastName)
+                 ->setPrenom($faker->firstName)
+                 ->setDateNaissance($faker->date)
+                 ->setGenre($faker->randomElement($array = array ('Homme', 'Femme','Non precis')))
+                 ->setGroupe($faker->word(3))
+                 ->setAdresse($faker->address)
+                 ->setTelephone1($faker->phoneNumber)
+                 ->setEmail($faker->email)
+                 ->setNomParents($faker->lastName)
+                 ->setPrenomParents($faker->firstName)
+                 ->setTelephoneParents1($faker->phoneNumber)
+                 ->setEmailParents($faker->email)
+                 ->setCotisation($faker->number)
+                 ->setDateInscription($faker->date)
+                 ->setGrade($faker->word(5))
+                 ->setObservation($faker->sentence(7))
+                 ->setMalade($faker->randomElement($array= array (true,false)))
+                 ->setCategorie($categorie)
+                 ->setInformationMedicale($infoMedicale)
+                 ->addActivite($activite);
+
           $manager-> persist($membre);
-        // $product = new Product();
-        // $manager->persist($product);
+        
       }
         $manager->flush();
     }
