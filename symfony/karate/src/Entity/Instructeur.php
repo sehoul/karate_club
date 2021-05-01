d54129a2c5553f836567a8aaff665a91185141e7
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-05-01 18:13:56 +0200

    Update Instructeur.php

--- symfony/karate/src/Entity/Instructeur.php
+++ symfony/karate/src/Entity/Instructeur.php
@@ -60,7 +60,7 @@ class Instructeur
     private $Tel_1;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, nullable=true)
      */
     private $Tel_2;
 
