b86bcd8a3609e3e25d08f72bb564ad6f3a7d8350
mohamedel0 <elidrissimohamed058@gmail.com>
2021-05-01 15:33:44 +0200

    Update AppFixtures.php

--- symfony/karate/src/DataFixtures/AppFixtures.php
+++ symfony/karate/src/DataFixtures/AppFixtures.php
@@ -17,27 +17,9 @@ class AppFixtures extends Fixture
       $faker= \Faker\Factory::create('fr_FR');
       for ($i=0;$i<4; $i++){
           $membre=new Membre();
-          $membre->setNom($faker->Nom)
-                 ->setPrenom($faker->Prenom)
-                 ->setDateNaissance($faker->DateNaissance)
-                 ->setGenre($faker->Genre)
-                 ->setGroupe($faker->Groupe)
-                 ->setAdresse($faker->Adresse)
-                 ->setTelephone1($faker->Telephone1)
-                 ->setTelephone2($faker->Telephone2)
-                 ->setNomParents($faker->NomParents)
-                 ->setPrenomParents($faker->PrenomParents)
-                 ->setTelephoneParents1($faker->TelephoneParents1)
-                 ->setTelephoneParents2($faker->TelephoneParents2)
-                 ->setEmailParents($faker->EmailParents)
-                 ->setCotisation($faker->Cotisation)
-                 ->setDateInscription($faker->DateInscription)
-                 ->setGrade($faker->Grade)
-                 ->setObservation($faker->Observation)
-                 ->setMalade($faker->malade)
-                 ->setCategorie($faker->categorie)
-                 ->setInformationMedicale($faker->informationMedicale)
-                 ->setActivites($faker->activites);
+          $membre->setNom($faker->word)
+                 ->setPrenom($faker->word);
+                 
           $manager-> persist($membre);
         // $product = new Product();
         // $manager->persist($product);
