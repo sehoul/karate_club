e960f12489c2030bb6bb4a6c5a7db37ee32c5279
mohamedel0 <elidrissimohamed058@gmail.com>
2021-05-02 12:39:57 +0200

    commit

---
+++ angular/karate/src/app/groupe.model.ts
@@ -0,0 +1,19 @@
+export class Groupe{
+
+
+ public id:number
+ public NomGroupe:string  
+ public Instructeur:string
+
+ public constructor(id:number
+    , NomGroupe:string
+    , Instructeur:string){
+
+        this.id = id
+        this.NomGroupe = NomGroupe
+        this.Instructeur = Instructeur
+    }
+}
+
+
+
--- angular/karate/src/app/president/addgroupe/addgroupe.component.ts
+++ angular/karate/src/app/president/addgroupe/addgroupe.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Groupe } from 'src/app/groupe.model';
 
 @Component({
   selector: 'app-addgroupe',
@@ -6,8 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addgroupe.component.css']
 })
 export class AddgroupeComponent implements OnInit {
+  formAA: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.formAA=this.fb.group({
+      NomGroupe:  new FormControl('', [Validators.required]),
+      Instructeur:new FormControl('', [Validators.required]),
+    });
+   }
+
+
+   get NomGroupe() : any {   return this.formAA.get('NomGroupe');}
+   get Instructeur() : any { return this.formAA.get('Instructeur');}
+   private groupe:Array<Groupe>=[new Groupe(1,"Groupe1","Mostafa")]
+
+   submit() {
+    console.log(this.formAA.getRawValue());
+    const data=this.formAA.getRawValue();
+    this.groupe.push(new Groupe(data.id,data.NomGroupe,data.Instructeur));
+    console.log(this.groupe);}
 
   ngOnInit(): void {
   }
