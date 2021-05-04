cdf85d98c8ee972b72534b2d11c87f6d2acab397
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-05-05 00:14:08 +0200

    commit

--- angular/karate/src/app/president/membres/membres.component.ts
+++ angular/karate/src/app/president/membres/membres.component.ts
@@ -7,6 +7,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MembresComponent implements OnInit {
 
+  userList = [
+
+    {
+    
+    "id": 1,
+    
+    "name": "Leanne Graham",
+    
+    "username": "Bret",
+    
+    "email": "Sincere@april.biz"
+    
+    },
+    
+    {
+    
+    "id": 2,
+    
+    "name": "Ervin Howell",
+    
+    "username": "Antonette",
+    
+    "email": "Shanna@melissa.tv"
+    
+    },
+    
+    {
+    
+    "id": 3,
+    
+    "name": "Clementine Bauch",
+    
+    "username": "Samantha",
+    
+    "email": "Nathan@yesenia.net"
+    
+    },
+    
+    {
+    
+    "id": 4,
+    
+    "name": "Patricia Lebsack",
+    
+    "username": "Karianne",
+    
+    "email": "Julianne.OConner@kory.org"
+    
+    },
+    
+    {
+    
+    "id": 5,
+    
+    "name": "Chelsey Dietrich",
+    
+    "username": "Kamren",
+    
+    "email": "Lucio_Hettinger@annie.ca"
+    
+    }
+    
+    ]
+
   constructor() { }
 
   ngOnInit(): void {
