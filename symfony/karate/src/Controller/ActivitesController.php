e9ac8a6003cc359339a8832db16d73f8024718fd
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-05-18 06:38:07 +0200

    Create ActivitesController.php

---
+++ symfony/karate/src/Controller/ActivitesController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Groupe;
+use App\Repository\ActiviteRepository;
+use App\Repository\GroupeRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
+
+class ActivitesController extends AbstractController
+{
+    public function __construct(ActiviteRepository $activiteRepository){
+        $this->activiteRepository=$activiteRepository;
+    }
+    /**
+     * @Route("/activites", name="Activites", methods={"GET"})
+     */
+    public function getActivites(): Response
+    {
+        return $this->json($this->activiteRepository->findAll(), 200, [],array('groups' => array('Groupe','Activite:Groupe','Activite')));
+    }
+}
