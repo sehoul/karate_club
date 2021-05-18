973a810f84b24e8de2a9f108bf9ac8f55672b2de
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-05-18 23:43:10 +0200

    Update CategoriesController.php

--- symfony/karate/src/Controller/CategoriesController.php
+++ symfony/karate/src/Controller/CategoriesController.php
@@ -6,6 +6,7 @@ use App\Entity\Categorie;
 use App\Repository\CategorieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 
@@ -19,6 +20,6 @@ class CategoriesController extends AbstractController
      */
     public function getCategories(): Response
     {
-        return $this->json($this->categorieRepository->findAll(), 200, [],array('groups' => array('categories')));
+        return $this->json($this->categorieRepository->findAll(), 200, [],[AbstractNormalizer::ATTRIBUTES => ['id','nomCategorie','Description']]);
     }
 }
