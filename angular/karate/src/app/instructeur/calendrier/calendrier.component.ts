f9461a685df8fc035a53c3e160d660c15a8910b8
Sehoul Taha <taha.sehoul@etu.univ-orleans.fr>
2021-05-19 01:05:35 +0200

    commit

--- angular/karate/src/app/instructeur/calendrier/calendrier.component.ts
+++ angular/karate/src/app/instructeur/calendrier/calendrier.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CalendarOptions } from '@fullcalendar/angular';
+import frLocale from '@fullcalendar/core/locales/fr';
 
 @Component({
   selector: 'app-calendrier',
@@ -9,6 +10,8 @@ import { CalendarOptions } from '@fullcalendar/angular';
 export class CalendrierComponent implements OnInit {
 
   calendarOptions: CalendarOptions = {
+    locales: [frLocale ],
+    locale: 'fr',
     initialView: 'dayGridMonth',
     dateClick: this.handleDateClick.bind(this), // bind is important!
     events: [
@@ -16,7 +19,7 @@ export class CalendrierComponent implements OnInit {
       { title: 'event 2', date: '2021-05-19' }
     ]
   };
-
+ 
   handleDateClick(arg: { dateStr: string; }) {
     alert('date click! ' + arg.dateStr)
   }
