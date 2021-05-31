f457b5efd454236b0caad1f9006d23f08b6da1d5
mohamedel0 <elidrissimohamed058@gmail.com>
2021-05-31 15:37:43 +0200

    commit

--- angular/karate/src/app/president/addemploidutemps/addemploidutemps.component.ts
+++ angular/karate/src/app/president/addemploidutemps/addemploidutemps.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { CookieService } from 'ngx-cookie-service';
+import { GroupesService } from 'src/app/Services/groupes.service';
+import { InstructeurService } from 'src/app/Services/instructeur.service';
 
 @Component({
   selector: 'app-addemploidutemps',
@@ -7,9 +11,79 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddemploidutempsComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  _success:string="";
+  _error:string="";
+  activiteService: any;
+  Activites: any;
+  Instructeurs: any;
+
+  constructor(private instructeurService:InstructeurService,private groupeService:GroupesService, private fb: FormBuilder,private cookie:CookieService) {
+    this.form=this.fb.group({
+      nomevenement:  new FormControl('', [Validators.required]),
+      dateE:new FormControl('', [Validators.required]),
+      Instructeur:new FormControl('', [Validators.required]),
+      Activite:new FormControl('', [Validators.required]),
+      Groupe:new FormControl('', [Validators.required]),
+    });
+  }
+ 
+
+  get nomevenement() : any {   return this.form.get('nomevenement');}
+  get dateE() : any { return this.form.get('dateE');}
+  get Instructeur() : any { return this.form.get('Instructeur');}
+  get Activite() : any { return this.form.get('Activite');}
+  get Groupe() : any { return this.form.get('Groupe');}
+
+  submit() {
+    const data={
+      nomevenement:this.form.getRawValue().nomevenement,
+      dateE:this.form.getRawValue().dateE,
+      Instructeur:{ 
+        id:Number(this.form.getRawValue().Instructeur)
+      },
+      Activite:{ 
+        id:Number(this.form.getRawValue().Activite)
+      },
+      Groupe:{ 
+        id:Number(this.form.getRawValue().Groupe)
+      },
+    }
+  
+    
+    if(data.nomevenement!="" && data.Instructeur.id && data.Activite){
+
+     /*this.groupeService.getGroupes(Number(this.cookie.get('idPres')),data).subscribe(
+        (res:any)=>{
+          this._success="Crenau ajoutée avec succes !";
+          this._error="";
+        },
+        (error: { error: { message: string; }; })=>{
+          this._success="";
+          this._error=error.error.message;
+        }
+      )
+    }else{
+      this._success="";
+      this._error="merci de remplire tous les champs";
+    }*/
+
+  }
+}
 
   ngOnInit(): void {
+    this.groupeService.getGroupes().subscribe((resp:any)=>{
+      this.Activites=resp;
+      console.log(resp);
+    });
+    this.instructeurService.getInstructeursMiniInfo().subscribe((resp:any)=>{
+      this.Instructeurs=resp;
+    });
+
+  }
   }
 
-}
+
+  
+
+
--- angular/karate/src/app/president/emploidutemps/emploidutemps.component.ts
+++ angular/karate/src/app/president/emploidutemps/emploidutemps.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CalendarOptions } from '@fullcalendar/angular';
+import frLocale from '@fullcalendar/core/locales/fr';
 
 @Component({
   selector: 'app-emploidutemps',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmploidutempsComponent implements OnInit {
 
+  calendarOptions: CalendarOptions = {
+    locales: [frLocale ],
+    locale: 'fr',
+    initialView: 'dayGridMonth',
+    dateClick: this.handleDateClick.bind(this), // bind is important!
+    events: [
+      { title: 'event 1', date: '2021-05-18' },
+      { title: 'event 2', date: '2021-05-19' }
+    ]
+  };
+ 
+  handleDateClick(arg: { dateStr: string; }) {
+    alert('date click! ' + arg.dateStr)
+  }
+  
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
